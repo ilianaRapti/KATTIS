d49bfe904e4e9fc74d7ac30adc0b080870fc7c9c
Iliana <106244484+ilianaRapti@users.noreply.github.com>
2024-08-17 12:41:19 +0300

    Add files via upload

--- aleidibio.c
+++ aleidibio.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c;
+    int a, b ,c;
+
+    printf("");
+    scanf("%d %d %d",&a,&b,&c);
+    int res = c-b-a;
+    printf("%d",res);
 
 
     return 0;
